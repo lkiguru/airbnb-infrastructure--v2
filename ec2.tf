c1e22619ef2254dd904410fb0418e9a284509c02
unknown <lonahkiguru@gmail.com>
2022-11-16 21:35:41 -0500

    error fix

--- ec2.tf
+++ ec2.tf
@@ -5,9 +5,8 @@ resource "aws_instance" "web" {
    monitoring = true  
    ebs_optimized = true
 }
-   root_block_device {
-   encrypted     = true
-   
+  
+    
   tags = {
     Name = "HelloWorld"
   }
